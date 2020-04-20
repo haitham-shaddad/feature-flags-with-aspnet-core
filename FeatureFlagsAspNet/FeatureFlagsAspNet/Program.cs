ca9c885176ab2fe10cfb06e914d836250441e5dd
Haitham Shaddad <haitham.shaddad@outlook.com>
2020-04-20 19:31:25 +1000

    Add azure app configuration

--- FeatureFlagsAspNet/FeatureFlagsAspNet/Program.cs
+++ FeatureFlagsAspNet/FeatureFlagsAspNet/Program.cs
@@ -18,6 +18,20 @@ namespace FeatureFlagsAspNet
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config =>
+                {
+                    var configuration = config.Build();
+                    config.AddAzureAppConfiguration(azureAppConfig =>
+                    {
+                        var azureAppConfigConnectionString = configuration["AzureAppConfig:ConnectionString"];
+                        azureAppConfig.ConfigureRefresh(refresh =>
+                        {
+                            refresh.SetCacheExpiration(TimeSpan.FromSeconds(5));
+                        });
+                        azureAppConfig.UseFeatureFlags();
+                        azureAppConfig.Connect(azureAppConfigConnectionString);
+                    });
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
